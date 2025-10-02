33515c9562b993769ce1d8dd8568d953832947f4
primika27 <kprimika@yahoo.com>
2025-10-02 13:21:16 -0400

    fix connection redis

--- src/store_manager.py
+++ src/store_manager.py
@@ -10,8 +10,10 @@ from orders.controllers.order_controller import create_order, remove_order, get_
 from orders.controllers.user_controller import create_user, remove_user, get_user
 from stocks.controllers.product_controller import create_product, remove_product, get_product
 from stocks.controllers.stock_controller import get_stock, set_stock, get_stock_overview
- 
+from sync_redis import sync_redis_with_mysql
+
 app = Flask(__name__)
+sync_redis_with_mysql()
 
 @app.get('/health-check')
 def health():
